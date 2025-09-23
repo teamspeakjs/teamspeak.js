f1bc382f568434a0689fef94a308d472e4756fec
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-09-23 13:42:43 +0200

    Fix param at Instance

--- src/structures/instance.ts
+++ src/structures/instance.ts
@@ -32,54 +32,52 @@ export class Instance extends Base {
    * Patches the instance with new data.
    * @param {Partial<RawInstance>} data The new data to patch the instance with.
    */
-  _patch(_data: Partial<RawInstance>): void {
-    if ('serverinstance_database_version' in _data) {
-      this.databaseVersion = Number(_data.serverinstance_database_version!);
+  _patch(data: Partial<RawInstance>): void {
+    if ('serverinstance_database_version' in data) {
+      this.databaseVersion = Number(data.serverinstance_database_version!);
     }
-    if ('serverinstance_filetransfer_port' in _data) {
-      this.filetransferPort = Number(_data.serverinstance_filetransfer_port!);
+    if ('serverinstance_filetransfer_port' in data) {
+      this.filetransferPort = Number(data.serverinstance_filetransfer_port!);
     }
-    if ('serverinstance_max_download_total_bandwidth' in _data) {
-      this.maxDownloadTotalBandwidth = Number(_data.serverinstance_max_download_total_bandwidth!);
+    if ('serverinstance_max_download_total_bandwidth' in data) {
+      this.maxDownloadTotalBandwidth = Number(data.serverinstance_max_download_total_bandwidth!);
     }
-    if ('serverinstance_max_upload_total_bandwidth' in _data) {
-      this.maxUploadTotalBandwidth = Number(_data.serverinstance_max_upload_total_bandwidth!);
+    if ('serverinstance_max_upload_total_bandwidth' in data) {
+      this.maxUploadTotalBandwidth = Number(data.serverinstance_max_upload_total_bandwidth!);
     }
-    if ('serverinstance_guest_serverquery_group' in _data) {
-      this.guestServerqueryGroup = Number(_data.serverinstance_guest_serverquery_group!);
+    if ('serverinstance_guest_serverquery_group' in data) {
+      this.guestServerqueryGroup = Number(data.serverinstance_guest_serverquery_group!);
     }
-    if ('serverinstance_serverquery_flood_commands' in _data) {
-      this.serverqueryFloodCommands = Number(_data.serverinstance_serverquery_flood_commands!);
+    if ('serverinstance_serverquery_flood_commands' in data) {
+      this.serverqueryFloodCommands = Number(data.serverinstance_serverquery_flood_commands!);
     }
-    if ('serverinstance_serverquery_flood_time' in _data) {
-      this.serverqueryFloodTime = Number(_data.serverinstance_serverquery_flood_time!);
+    if ('serverinstance_serverquery_flood_time' in data) {
+      this.serverqueryFloodTime = Number(data.serverinstance_serverquery_flood_time!);
     }
-    if ('serverinstance_serverquery_ban_time' in _data) {
-      this.serverqueryBanTime = Number(_data.serverinstance_serverquery_ban_time!);
+    if ('serverinstance_serverquery_ban_time' in data) {
+      this.serverqueryBanTime = Number(data.serverinstance_serverquery_ban_time!);
     }
-    if ('serverinstance_template_serveradmin_group' in _data) {
-      this.templateServeradminGroup = Number(_data.serverinstance_template_serveradmin_group!);
+    if ('serverinstance_template_serveradmin_group' in data) {
+      this.templateServeradminGroup = Number(data.serverinstance_template_serveradmin_group!);
     }
-    if ('serverinstance_template_serverdefault_group' in _data) {
-      this.templateServerdefaultGroup = Number(_data.serverinstance_template_serverdefault_group!);
+    if ('serverinstance_template_serverdefault_group' in data) {
+      this.templateServerdefaultGroup = Number(data.serverinstance_template_serverdefault_group!);
     }
-    if ('serverinstance_template_channeladmin_group' in _data) {
-      this.templateChanneladminGroup = Number(_data.serverinstance_template_channeladmin_group!);
+    if ('serverinstance_template_channeladmin_group' in data) {
+      this.templateChanneladminGroup = Number(data.serverinstance_template_channeladmin_group!);
     }
-    if ('serverinstance_template_channeldefault_group' in _data) {
-      this.templateChanneldefaultGroup = Number(
-        _data.serverinstance_template_channeldefault_group!,
-      );
+    if ('serverinstance_template_channeldefault_group' in data) {
+      this.templateChanneldefaultGroup = Number(data.serverinstance_template_channeldefault_group!);
     }
-    if ('serverinstance_permissions_version' in _data) {
-      this.permissionsVersion = Number(_data.serverinstance_permissions_version!);
+    if ('serverinstance_permissions_version' in data) {
+      this.permissionsVersion = Number(data.serverinstance_permissions_version!);
     }
-    if ('serverinstance_pending_connections_per_ip' in _data) {
-      this.pendingConnectionsPerIp = Number(_data.serverinstance_pending_connections_per_ip!);
+    if ('serverinstance_pending_connections_per_ip' in data) {
+      this.pendingConnectionsPerIp = Number(data.serverinstance_pending_connections_per_ip!);
     }
-    if ('serverinstance_serverquery_max_connections_per_ip' in _data) {
+    if ('serverinstance_serverquery_max_connections_per_ip' in data) {
       this.serverqueryMaxConnectionsPerIp = Number(
-        _data.serverinstance_serverquery_max_connections_per_ip!,
+        data.serverinstance_serverquery_max_connections_per_ip!,
       );
     }
   }
